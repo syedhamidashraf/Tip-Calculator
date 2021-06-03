ba5ca6bb36955ca19fdfeb9ccb7e984cca83849f
Syed Hamid Ashraf <syedhamidashraf@gmail.com>
2021-06-03 09:56:46 +0530

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(26, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(37, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.tip_Decrementer);
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button.Location = new System.Drawing.Point(209, 122);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(36, 34);
+            this.button.TabIndex = 4;
+            this.button.Text = "+";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.tip_Incrementer);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(209, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(36, 34);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.people_Incrementer);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(26, 201);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(37, 34);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.people_Decrementer);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(440, 77);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(156, 28);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "$0.00";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(440, 154);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(156, 28);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "$0.00";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 13F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Location = new System.Drawing.Point(23, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Bill Amount";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 13F);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label3.Location = new System.Drawing.Point(24, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 23);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Tip %";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 13F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label2.Location = new System.Drawing.Point(22, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 23);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Number of people";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(294, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 30);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Tip";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(294, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 30);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Total";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(169, 268);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 28);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Calculate";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.calculate);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button6.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(319, 268);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(104, 28);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "Reset";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.reset);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.textBox1.Location = new System.Drawing.Point(26, 45);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 33);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.Text = "0.00";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.textBox2.Location = new System.Drawing.Point(62, 123);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 33);
+            this.textBox2.TabIndex = 18;
+            this.textBox2.Text = "10";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.textBox3.Location = new System.Drawing.Point(62, 202);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(149, 33);
+            this.textBox3.TabIndex = 19;
+            this.textBox3.Text = "1";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label6.Location = new System.Drawing.Point(295, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 19);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "per person";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label7.Location = new System.Drawing.Point(295, 182);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 19);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "per person";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(632, 308);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tip Calculator";
+            this.TransparencyKey = System.Drawing.Color.Tomato;
+            this.Load += new System.EventHandler(this.TipCalculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public static float SetValueForText1 = 0; // variable for Bill value
+        public static int SetValueForText2 = 0; // variable for Tip % value
+        public static int SetValueForText3 = 0; // variable for number of person value
+
+        public static float tip = 0;              // tip
+        public static float tipPerPerson = 0;     // tip per person
+        public static float totPerPerson = 0;     // total per person
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void TipCalculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculate(object sender, EventArgs e)  //calculate button
+        {
+            textBox1.Text = Math.Round(float.Parse(textBox1.Text), 2).ToString();  // Rounding Bill to only 2 decimal places
+            SetValueForText1 = float.Parse(textBox1.Text);
+
+
+            SetValueForText2 = int.Parse(textBox2.Text); 
+
+            SetValueForText3 = int.Parse(textBox3.Text);
+            
+
+            tip = SetValueForText1 * SetValueForText2 / 100;   // calculating tip
+
+            tipPerPerson = tip / SetValueForText3;
+            tipPerPerson = (float)Math.Round(tipPerPerson, 2);    // Calculating tip per person and rounding it to 2 decimal places
+
+            totPerPerson = (SetValueForText1 + tip) / SetValueForText3;   // calculating total amount per person and rounding it to 2 decimal places
+            totPerPerson = (float)Math.Round(totPerPerson, 2);
+
+            textBox4.Text = "$" + (tipPerPerson.ToString());
+            textBox5.Text = "$" + (totPerPerson.ToString());
+        }
+
+        private void tip_Incrementer(object sender, EventArgs e) // + button to increase 1 value in Tip
+        {
+            if (int.Parse(textBox2.Text) < 100)
+            {
+                textBox2.Text = (int.Parse(textBox2.Text) + 1).ToString();
+            }
+        }
+
+        private void tip_Decrementer(object sender, EventArgs e) // - button to decrease 1 value in Tip
+        {
+            if (int.Parse(textBox2.Text) > 0)
+            {
+                textBox2.Text = (int.Parse(textBox2.Text) - 1).ToString();
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)  // Bill textbox
+       {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, "[^0-9.-]+"))  // To check if the number is floating point
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+            }
+
+            float value;
+            if (float.TryParse(textBox1.Text, out value))    // setting the max and min value of Bill
+            {
+                if (value > 100000)
+                    textBox1.Text = "0.00";
+                else if (value <0 )
+                    textBox1.Text = "0.00";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)   // Tip %
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox2.Text, "[^0-9]"))  
+            {
+                textBox2.Text = textBox2.Text.Remove(textBox2.Text.Length - 1);
+            }
+
+            int value;
+            if (int.TryParse(textBox2.Text, out value))
+            {
+                if (value > 100)
+                    textBox2.Text = "100";
+                else if (value < 0)
+                    textBox2.Text = "0";
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)   // Number of people
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox3.Text, "[^0-9]"))
+            {
+                textBox3.Text = textBox2.Text.Remove(textBox3.Text.Length - 1);
+            }
+
+            int value;
+            if (int.TryParse(textBox3.Text, out value))
+            {
+                if (value > 1000)
+                    textBox3.Text = "1000";
+                else if (value < 1)
+                    textBox3.Text = "1";
+            }
+        }
+
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void people_Incrementer(object sender, EventArgs e) // + button to increase 1 value in No of people
+        {
+            if (int.Parse(textBox3.Text) < 1000)
+            {
+                textBox3.Text = (int.Parse(textBox3.Text) + 1).ToString();
+            }
+        }
+
+        private void people_Decrementer(object sender, EventArgs e)  // - button to decrease 1 value in No of people
+        {
+            if (int.Parse(textBox3.Text) > 1)
+            {
+                textBox3.Text = (int.Parse(textBox3.Text) - 1).ToString();
+            }
+            
+        }
+
+        
+
+        private void reset(object sender, EventArgs e) // reset button
+        {
+            textBox1.Text = "0.00";
+            textBox2.Text = "10";
+            textBox3.Text = "1";
+            textBox4.Text = "$0.00";
+            textBox5.Text = "$0.00";
+        }
+
+    }
+}
